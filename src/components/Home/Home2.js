da39ba8d7d89802f5dfe99255234d37bb5ba950c
Austinae <guerrandw@gmail.com>
2024-02-05 16:46:55 +0100

    v2.1.2 Refactoring main page introduction text

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -23,14 +23,13 @@ const Home2 = () => {
               </Tilt>
             </div>
             <div className="introduction-text">
-              <p className="home-about-body">Raised in France, I graduated from a <span className="blue"> bilingual</span> school with an OIB-British section Baccalaureate and earned a <span className="blue"> Computer Science degree</span> with high honors in the UK in 2021.</p>
-              <p className="home-about-body">I've always been interested in computers and I learned to type on a keyboard before I could properly write.</p>
-              <p className="home-about-body">My aim is to<b className="blue"> build new technologies </b>for a sustainable future.</p>
-              <p className="home-about-body">On top of my studies, I worked on projects like Musion, Treeco, and the IBM AI Academic Initiative to further my skills. </p>
-              <p className="home-about-body">After completing my studies, I had the option to continue in academia or start working. Because I had such rewarding experiences collaborating with professional engineers, I chose to enter the workforce to further my practical learning.</p>
-              <p className="home-about-body">Since graduating, I've worked as a Freelancer and a Full-Time engineer, specializing in roles ranging from Web Development at CTH and to AI NLP Engineering at Storya and Full Stack Engineering at talent.io.</p>
-              <p className="home-about-body"><b className="blue"> I uphold high standards of quality and meticulous attention to detail</b> in my work, aiming to drive meaningful impact within any organization I join.</p>
-              <p className="home-about-body">Be the change you want to see in the world.</p>
+              <p className="home-about-body">I'm William Guerrand, hailing from the vibrant city of Paris, where my upbringing was enriched by the fusion of Chicagoan and Norman influences from my parents. This unique background immersed me in diverse cultures, equipping me with <b className="blue">fluency in both French and English.</b></p>
+              <p className="home-about-body">Early on I was fascinated by technology. I saw the transformative impact of portable phones and computers and how the internet bubble revolutioned lives and businesses. As this unfolded, I was tinkering with games and seeing how I could modify the source code to make it do what I wished. I thought that if I wanted to make an impact this could be something I could do in the future.</p>
+              <p className="home-about-body">This dream quickly became a reality when at the end of high school I flew across the English Channel to <b className="blue">study Computer Science at Lancaster University</b>. On top of my studies, I worked on projects like Musion, Treeco, and the IBM AI Academic Initiative to further my skills.</p>
+              <p className="home-about-body">Post-graduation, I faced the pivotal decision between continuing in academia or entering the professional realm. However, I had learned so much by collaborating with professional engineers that I thought it best to jump right in.</p>
+              <p className="home-about-body">Since then, I've donned various hats, working as a Freelancer and Full-Time Engineer. My roles have spanned Web Development at CTH, AI NLP Engineering at Storya, and Full Stack Engineering at talent.io.</p>
+              <p className="home-about-body">Today, I like to introduce myself as a <b className="blue">Full-Stack Developer</b>  <b className="blue">AI Engineer</b>, channeling my skills to contribute to the dynamic technological advancements within the organizations I engage with. <b className="blue">I uphold high standards of quality and meticulous attention to detail</b> in my work, aiming to drive meaningful impact within any organization I join. In my world, I believe in being the change one wishes to see, and I carry this ethos into every endeavor I undertake.</p>
+              <p className="home-about-body">Godspeed.</p>
             </div>
           </div>
         </Row>
